ba077abbfdc27fddd255b88ee65d3d1020599405
Ciro Neto <ciro.neto16@gmail.com>
2022-02-04 23:15:51 -0300

    create service and controller methods for the transactions

--- src/main/java/dev/cironeto/jsonmapping/controller/TransactionController.java
+++ src/main/java/dev/cironeto/jsonmapping/controller/TransactionController.java
@@ -1,9 +1,23 @@
 package dev.cironeto.jsonmapping.controller;
 
+import dev.cironeto.jsonmapping.dto.TransactionDto;
+import dev.cironeto.jsonmapping.service.TransactionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping(value = "/transactions")
+@RequiredArgsConstructor
 public class TransactionController {
+    private final TransactionService transactionService;
+
+    @GetMapping
+    public ResponseEntity<List<TransactionDto>> findAll() {
+        return ResponseEntity.ok(transactionService.findAll());
+    }
 }
--- src/main/java/dev/cironeto/jsonmapping/service/TransactionService.java
+++ src/main/java/dev/cironeto/jsonmapping/service/TransactionService.java
@@ -1,12 +1,59 @@
 package dev.cironeto.jsonmapping.service;
 
+import dev.cironeto.jsonmapping.dto.TransactionDto;
+import dev.cironeto.jsonmapping.entity.Transaction;
+import dev.cironeto.jsonmapping.repository.TransactionRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
+@RequiredArgsConstructor
 public class TransactionService {
+    private final TransactionRepository transactionRepository;
 
-//    @Transactional
+    @Transactional
+    public TransactionDto save(TransactionDto dto) {
+        Transaction entity = new Transaction();
+        convertDtoToEntity(entity, dto);
+        entity = transactionRepository.save(entity);
+        return new TransactionDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<TransactionDto> findAll() {
+        List<Transaction> list = transactionRepository.findAll();
+
+        List<TransactionDto> dtoList = new ArrayList<>();
+
+        for (Transaction transaction : list) {
+            TransactionDto dto = new TransactionDto(transaction);
+            dtoList.add(dto);
+        }
+        return dtoList;
+    }
 
 
+    private void convertDtoToEntity(Transaction entity, TransactionDto dto) {
+        entity.setId(dto.getId());
+        entity.setArrangementId(dto.getArrangementId());
+        entity.setBookingDate(dto.getBookingDate());
+        entity.setType(dto.getType());
+        entity.setValueDate(dto.getValueDate());
+        entity.setAmount(dto.getAmount());
+        entity.setCurrencyCode(dto.getCurrencyCode());
+        entity.setCurrency(dto.getCurrency());
+        entity.setCreditDebitIndicator(dto.getCreditDebitIndicator());
+        entity.setRunningBalance(dto.getRunningBalance());
+        entity.setCounterPartyAccountNumber(dto.getCounterPartyAccountNumber());
+        entity.setReference(dto.getReference());
+        entity.setTypeGroup(dto.getTypeGroup());
+        entity.setInstructedAmount(dto.getInstructedAmount());
+        entity.setUserKey(dto.getUserKey());
+
+
+    }
 }
