b69e6e866752b33e1f900b3a78b8ff2d4b482a55
Ciro Neto <ciro.neto16@gmail.com>
2022-02-04 23:18:42 -0300

    set Run method to persist the transactions from json file

--- src/main/java/dev/cironeto/jsonmapping/ConvertJsonApplication.java
+++ src/main/java/dev/cironeto/jsonmapping/ConvertJsonApplication.java
@@ -1,18 +1,18 @@
 package dev.cironeto.jsonmapping;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import dev.cironeto.jsonmapping.entity.InputTransaction;
-import dev.cironeto.jsonmapping.entity.Transaction;
-import dev.cironeto.jsonmapping.repository.TransactionRepository;
+import dev.cironeto.jsonmapping.dto.TransactionDto;
+import dev.cironeto.jsonmapping.dto.InputTransaction;
+import dev.cironeto.jsonmapping.service.TransactionService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.io.IOException;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -20,24 +20,31 @@ import java.util.List;
 @Slf4j
 @RequiredArgsConstructor
 public class ConvertJsonApplication implements CommandLineRunner {
-    private final TransactionRepository transactionRepository;
+    private final TransactionService transactionService;
 
-    public static void main(String[] args)  {
+
+    public static void main(String[] args) {
         SpringApplication.run(ConvertJsonApplication.class, args);
     }
+
 
     @Override
     public void run(String... args) throws IOException {
 
         ObjectMapper mapper = new ObjectMapper();
-        List<InputTransaction> transactions = Arrays.asList(mapper.readValue(Paths.get("transactions.json")
-                .toFile(), InputTransaction[].class));
+        List<InputTransaction> inputTransactionList = Arrays
+                .asList(mapper.readValue(Paths.get("transactions.json")
+                        .toFile(), InputTransaction[].class));
 
-        transactions.forEach(it -> log.info(it.toString()));
-        log.info("mapping");
+        List<TransactionDto> dtoList = new ArrayList<>();
+        for (InputTransaction inputTransaction : inputTransactionList) {
+            TransactionDto transactionDto = new TransactionDto(inputTransaction);
+            dtoList.add(transactionDto);
+        }
 
-
-//        transactionRepository.save();
+        for (TransactionDto transactionDto : dtoList) {
+            transactionService.save(transactionDto);
+        }
 
     }
 }
